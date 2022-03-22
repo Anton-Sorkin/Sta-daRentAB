e7042087ce35936fc99165d3e9842357c485ec4f
Anton <anton.sorkin98@outlook.com>
2022-03-22 13:33:42 +0100

    no changes

---
+++ utils/utils.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+
+const hashPassword = (password) => {
+  const hash = bcrypt.hashSync(password, 8);
+  return hash;
+};
+
+const comparePassword = (password, hash) => {
+  const correct = bcrypt.compareSync(password, hash);
+  return correct;
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
