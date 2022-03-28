e57189f08fb61b02f07ea7f3dc5bc0d003400fff
Anton <anton.sorkin98@outlook.com>
2022-03-28 11:46:35 +0200

    added admin hostory

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,11 +1,38 @@
 //REQUIREMENTS
 const express = require("express");
 const staffModel = require("../models/staffModel");
+const BookingModel = require("../models/BookingModel");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 //GETS
-router.get("/", (req, res) => {
-  res.render("admin-page");
+
+//get admin page with users
+router.get("/", async (req, res) => {
+  const staff = await staffModel.find().lean();
+  const bookings = await BookingModel.find().lean();
+
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    res.render("admin-page", { staff, bookings });
+  } else {
+    res.redirect("/admin");
+  }
+});
+
+//get admin info page with users
+router.get("/info/:id", async (req, res) => {
+  const staff = await staffModel.find().lean();
+  const bookings = await BookingModel.find().lean();
+
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    res.render("staff-admin-info", { bookings });
+  } else {
+    res.redirect("/admin");
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -25,8 +52,12 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/info", (req, res) => {
-  res.render("staff-admin-info");
+//post a change to value to info/id to mark it as done
+router.post("/info/:id", async (req, res) => {
+  const { isDone } = req.body;
+  await staffModel.findByIdAndUpdate(req.params.id, { isDone });
+  //inte klar|||||
+  res.redirect("/admin");
 });
 
 //EXPORTS
