b685bc2f82f12a5b094e0e90a595f19106872994
Anton <anton.sorkin98@outlook.com>
2022-03-28 09:17:59 +0200

    admi

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,38 +1,61 @@
 //REQUIREMENTS
 const express = require("express");
-const { default: mongoose } = require("mongoose");
 const staffModel = require("../models/staffModel");
 const router = express.Router();
-
-
+const jwt = require("jsonwebtoken");
 
 //GETS
-router.get("/", (req, res) => {
-  res.render("admin-page");
+
+//get admin page with users
+router.get("/", async (req, res) => {
+  const staff = await userModel.find().lean();
+
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    res.render("admin-page", { staff });
+  } else {
+    res.redirect("/admin");
+  }
 });
 
+//get admin info page with users
+router.get("/info", async (req, res) => {
+  const staff = await userModel.find().lean();
+
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    res.render("staff-admin-info", { staff });
+  } else {
+    res.redirect("/admin");
+  }
+});
+
+//post create new staff from admin
 router.post("/", async (req, res) => {
-
   const newStaff = new staffModel({
-      userName: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-      role: "staff"
-    });
+    userName: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    role: "staff",
+  });
 
-    if(req.body.password === req.body.password2){
+  if (req.body.password === req.body.password2) {
     const result = await newStaff.save();
 
     res.redirect("/admin");
-  
-    }
-    else{
-      res.sendStatus(400)
-    }
-})
+  } else {
+    res.sendStatus(400);
+  }
+});
 
-router.get("/info", (req, res) => {
-  res.render("staff-admin-info");
+//post a change to value to info/id to mark it as done
+router.post("/info/:id", async (req, res) => {
+  const { isDone } = req.body;
+  await staffModel.findByIdAndUpdate(req.params.id, { isDone });
+  //inte klar|||||
+  res.redirect("/admin");
 });
 
 //EXPORTS
